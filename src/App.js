930f7f5b154d25ab27c3d7b8e2abd2ea50f00883
Alex Liu <liuzhendell@gmail.com>
2021-01-12 16:34:39 -0700

    setup the project and redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import HomePage from "./components/HomePage/HomePage";
+import LoginPage from "./components/LoginPage/LoginPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+
+
+              <div style={{marginTop:"20px"}}>
+                  <Switch>
+                      <Route exact path="/" component={HomePage} />
+                      <Route exact path="/login" component={LoginPage} />
+                  </Switch>
+              </div>
+
+      </BrowserRouter>
   );
 }
 
