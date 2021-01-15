2a406102fa95f92fb963f587a27603fb8d3ea1f5
Alex Liu <liuzhendell@gmail.com>
2021-01-15 09:53:03 -0700

    update the login page

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,21 +1,36 @@
-import React,{useState} from "react";
-import {Col, Form, Input, Layout, Row, Radio, Button} from "antd";
+import React,{useState,useEffect} from "react";
+import {Col, Form, Input, Layout, Row, Radio, Button, Image, Card} from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import 'antd/dist/antd.css';
 import styles from './LoginPage.module.css';
 import {BorderOutlined, LockOutlined, MailOutlined, NumberOutlined, UserOutlined} from "@ant-design/icons";
+import logo from './vivaji_logo.png';
+import {login} from '../../actions/userActions';
 
 
 const {Header, Content, Footer} = Layout;
 
 const LoginPage = () => {
     const [conditionForm, setConditionForm] = useState("subscribe");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const dispatch = useDispatch();
+    const userLogin = useSelector((state) => state.userLogin);
+    const { loading, error, userInfo } = userLogin;
+    const history = useHistory();
     const onChange =e=>{
         setConditionForm(e.target.value)
     }
+    const submitHandler = () => {
+        /*e.preventDefault();*/
+        dispatch(login(email, password));
+        console.log("submit")
+    };
     let formForRender;
     if(conditionForm=="subscribe")
     {
-        formForRender=  <Form>
+        formForRender=  <Form onFinish={submitHandler}>
             <Form.Item
                 name="username"
                 rules={[
@@ -25,7 +40,7 @@ const LoginPage = () => {
                     },
                 ]}
             >
-                <Input prefix={<UserOutlined className="site-form-item-icon"/>} placeholder="Subscriber ID"/>
+                <Input className={styles.inputRight} prefix={<UserOutlined className="site-form-item-icon"/>} placeholder="Subscriber ID"/>
             </Form.Item>
             <Form.Item
                 name="password"
@@ -37,6 +52,7 @@ const LoginPage = () => {
                 ]}
             >
                 <Input.Password
+                    className={styles.inputRight}
                     prefix={<LockOutlined className="site-form-item-icon"/>}
                     type="password"
                     placeholder="Password"
@@ -65,7 +81,7 @@ const LoginPage = () => {
                   ]}
               >
 
-                  <Input prefix={<MailOutlined className="site-form-item-icon"/>} placeholder="Email"/>
+                  <Input className={styles.inputRight} prefix={<MailOutlined className="site-form-item-icon"/>} placeholder="Email"/>
               </Form.Item>
               <Form.Item
                   name="password"
@@ -77,6 +93,7 @@ const LoginPage = () => {
                   ]}
               >
                   <Input.Password
+                      className={styles.inputRight}
                       prefix={<LockOutlined className="site-form-item-icon"/>}
                       type="password"
                       placeholder="Password"
@@ -107,7 +124,7 @@ const LoginPage = () => {
                    },
                ]}
            >
-               <Input prefix={<NumberOutlined className="site-form-item-icon"/>} placeholder="Voucher Code"/>
+               <Input className={styles.inputRight} prefix={<NumberOutlined className="site-form-item-icon"/>} placeholder="Voucher Code"/>
            </Form.Item>
 
            <Form.Item>
@@ -130,7 +147,7 @@ const LoginPage = () => {
                     },
                 ]}
             >
-                <Input prefix={<BorderOutlined className="site-form-item-icon"/>} placeholder="Activation Code"/>
+                <Input className={styles.inputRight} prefix={<BorderOutlined className="site-form-item-icon"/>} placeholder="Activation Code"/>
             </Form.Item>
 
             <Form.Item>
@@ -152,7 +169,7 @@ const LoginPage = () => {
                     },
                 ]}
             >
-                <Input prefix={<MailOutlined className="site-form-item-icon"/>} placeholder="Activation Code"/>
+                <Input className={styles.inputRight} prefix={<MailOutlined className="site-form-item-icon"/>} placeholder="Activation Code"/>
             </Form.Item>
 
             <Form.Item>
@@ -167,24 +184,28 @@ const LoginPage = () => {
     return (
         <>
             <Layout className={`${styles.bgImage} layout`}>
-                <Content className={styles.centerArea}>
+
                     <Row type="flex" justify="center" align="middle" className={styles.centerRow} >
-                        <Col>
-                           <h2>VIVAJI</h2>
-                           <Radio.Group  buttonStyle="solid" defaultValue="subscribe" onChange={onChange} value={conditionForm}>
+                        <Col span={12} >
+                          <Card bordered={false} className={styles.leftForm}>
+                           <Radio.Group  buttonStyle="solid" defaultValue="subscribe" onChange={onChange} value={conditionForm} className={styles.buttonGroup}>
+                               <img width={"50%"} src={logo}/>
                                <Radio.Button className={styles.radioStyle} value="subscribe">Subscriber ID</Radio.Button>
                                <Radio.Button className={styles.radioStyle} value="email">Email</Radio.Button>
                                <Radio.Button className={styles.radioStyle} value="voucher">Voucher Code</Radio.Button>
                                <Radio.Button className={styles.radioStyle} value="activation">Activation Code</Radio.Button>
                                <Radio.Button className={styles.radioStyle} value="trial">Start Trial</Radio.Button>
                            </Radio.Group>
+                          </Card>
                         </Col>
-                       <Col>
+                       <Col span={12} >
+                           <Card bordered={false} className={styles.rightForm}>
                            {formForRender}
+                           </Card>
                        </Col>
                     </Row>
 
-                </Content>
+
 
             </Layout>
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
 import {combineReducers} from "redux";
+import {userLoginReducer} from "./userReducer";
 
-const rootReducers = combineReducers({});
+const rootReducers = combineReducers({
+    userLogin:userLoginReducer
+});
 
 export default rootReducers;
